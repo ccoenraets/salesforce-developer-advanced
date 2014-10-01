ae47d05436e683eb5a42acda7d14f9e69a6b7f52
ccoenraets <ccoenraets@gmail.com>
2014-10-01 11:07:17 -0400

    new assets

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
 var express = require('express'),
-    http = require('http'),
-    path = require('path'),
-    bodyParser     = require('body-parser'),
-    methodOverride = require('method-override'),
     request = require('request'),
+    bodyParser = require('body-parser'),
     app = express();
 
-app.use(bodyParser());          // pull information from html in POST
-app.use(methodOverride());      // simulate DELETE and PUT
-
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/client'));
 
-app.all('/proxy', function(req, res) {
-    var url = req.header('SalesforceProxy-Endpoint');
-    console.log("proxying: " + url);
-    request({ url: url, method: req.method, json: req.body, headers: {'Authorization': req.header('X-Authorization')} }).pipe(res);
+app.all('*', function (req, res, next) {
+
+        var targetURL = req.header('Target-URL');
+        if (!targetURL) {
+            res.send(500, { error: 'There is no Target-Endpoint header in the request' });
+            return;
+        }
+        request({ url: targetURL + req.url, method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
+            function (error, response, body) {
+                if (error) {
+                    console.error('error: ' + response.statusCode)
+                }
+            }).pipe(res);
 });
 
 app.set('port', process.env.PORT || 3000);
 
 app.listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
+    console.log('Proxy server listening on port ' + app.get('port'));
 });
